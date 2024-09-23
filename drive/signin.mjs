bf59f7209b83999f92026643d12b032e49f6fa53
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2024-09-23 11:13:38 +0900

    Update signin.mjs

--- drive/signin.mjs
+++ drive/signin.mjs
@@ -2,7 +2,7 @@ export default function signin ({clientId}) {
   const endpoint = 'https://accounts.google.com/o/oauth2/v2/auth'
   const search = '?' + new URLSearchParams({
     client_id: clientId,
-    redirect_uri: 'http://localhost:8080/',
+    redirect_uri: 'https://hcratch3.github.io/ControlPanel/drive/',
     response_type: 'token',
     scope: 'https://www.googleapis.com/auth/drive.file',
   })
