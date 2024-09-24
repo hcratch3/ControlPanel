33dd6c0a064a5c17d906a20b6885700177db8f87
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2024-09-24 15:46:51 +0900

    Update file.js

--- drive/file.js
+++ drive/file.js
@@ -3,6 +3,17 @@ function onChange(event) {
   let files = event.target.files;
   for (let file of files) {
     console.log(file.name); // [object file]...
+
+    let file_reader = new FileReader();
+
+    // ファイルの読み込みを行ったら実行
+    file_reader.addEventListener('load', function(e) {
+      console.log(e.target.result);
+                                    // FileReaderを使った
+                                    // テキストファイルからの読み込みテスト
+    });
+
+    file_reader.readAsText(e.target.files[0]);
   }
 }
 document.getElementById("file").addEventListener('change', onChange, false);
