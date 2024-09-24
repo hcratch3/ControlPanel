59ef97699e6d783c339b42778fd859e7ad0bb0cc
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2024-09-24 16:08:38 +0900

    Update file.js

--- drive/file.js
+++ drive/file.js
@@ -1,19 +1,33 @@
-function onChange(event) {
+async function onChange(event) {
   // 複数選択したファイルをFileList型オブジェクトとして取得
   let files = event.target.files;
+
   for (let file of files) {
     console.log(file.name); // ファイル名を出力
 
+    const fileContent = await readFile(file);
+    console.log(fileContent); // ファイルの内容を出力
+  }
+}
+
+// ファイルをテキストとして読み込む関数
+function readFile(file) {
+  return new Promise((resolve, reject) => {
     let fileReader = new FileReader();
 
-    // ファイルの読み込みが完了したときの処理
+    // 読み込みが完了したときの処理
     fileReader.onload = function(event) {
-      console.log(event.target.result); // ファイルの内容を出力
+      resolve(event.target.result); // 読み込んだ内容を返す
+    };
+
+    // 読み込みエラーの処理
+    fileReader.onerror = function(event) {
+      reject(new Error("ファイルの読み込み中にエラーが発生しました。"));
     };
 
     // ファイルをテキストとして読み込む
     fileReader.readAsText(file);
-  }
+  });
 }
 
 document.getElementById("file").addEventListener('change', onChange, false);
